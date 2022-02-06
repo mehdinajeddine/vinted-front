6c4e94305b6c4039519ef7c83e2e8436e3eb6b09
mehdinajeddine <mehdi.najeddine@void.fr>
2022-02-06 01:26:58 +0100

    clean strict mode

--- src/containers/Signin.js
+++ src/containers/Signin.js
@@ -66,9 +66,9 @@ const Signin = ({ logged, setLogged }) => {
 
         <div className="flex items-center justify-between">
           <div className="text-sm">
-            <a className="font-medium text-teal-600 hover:text-teal-500">
+            <button className="font-medium text-teal-600 hover:text-teal-500">
               Forgot your password?
-            </a>
+            </button>
           </div>
         </div>
 
